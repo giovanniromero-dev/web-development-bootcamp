cd1a5d3c9aea176cfadb27a18076ea233ddf9d5c
Giovanni <giovannidevelopments@gmail.com>
2025-01-18 17:55:56 +0100

    all ok

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// let generateName = require("sillyname");
+
+// let sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`)
+
+import {randomSuperhero} from "superheroes";
+
+const name = randomSuperhero();
+
+console.log(`I am ${name}!`)
